50b929617345f228b1e5b934a068513c7b4d5730
ggujangi <asmwj3@naver.com>
2021-04-01 02:49:47 +0900

    [submit 2] #12948

--- src/solution/Ex_12948.java
+++ src/solution/Ex_12948.java
@@ -7,13 +7,6 @@ package solution;
 
 public class Ex_12948 {
     public String solution(String phone_number) {
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 0; i < phone_number.length(); i++) {
-            if (i >= phone_number.length() - 4) answer.append(phone_number.charAt(i));
-            else answer.append("*");
-
-        }
-        return answer.toString();
+        return phone_number.replaceAll(".(?=.{4})", "*");
     }
 }
