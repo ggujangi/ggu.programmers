3ad3ad374b2ae9c82bcd9d8bd90d8002c9139875
ggujangi <asmwj3@naver.com>
2021-04-02 04:28:38 +0900

    [submit 2] #12935

--- src/solution/Ex_12935.java
+++ src/solution/Ex_12935.java
@@ -1,5 +1,7 @@
 package solution;
 
+import java.util.Arrays;
+
 /**
  * #12935
  * 제일 작은 수 제거하기
@@ -7,26 +9,8 @@ package solution;
 
 public class Ex_12935 {
     public int[] solution(int[] arr) {
-        int length = arr.length;
-        int minIndex = 0;
-
-        if (length == 1) return new int[]{-1};
-        int[] answer = new int[length - 1];
-
-        for (int i = 0; i < length; i++) {
-            if (arr[i] < arr[minIndex]) {
-                minIndex = i;
-            }
-        }
-
-        for (int i = minIndex + 1; i < length; i++) {
-            arr[i - 1] = arr[i];
-        }
-
-        for (int i = 0; i < length - 1; i++) {
-            answer[i] = arr[i];
-        }
-
-        return answer;
+        if (arr.length <= 1) return new int[]{-1};
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
     }
 }
