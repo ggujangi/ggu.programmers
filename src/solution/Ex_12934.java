2c5ce30351cb631d7e69b998a7c4419649d7d11a
ggujangi <asmwj3@naver.com>
2021-04-02 04:45:06 +0900

    [submit 1] #12934

---
+++ src/solution/Ex_12934.java
@@ -0,0 +1,13 @@
+package solution;
+
+/**
+ * #12934
+ * 정수 제곱근 판별
+ */
+
+public class Ex_12934 {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        return (sqrt - (int) sqrt) > 0 ? -1 : (long) Math.pow(sqrt + 1, 2);
+    }
+}
