4925e30aa738cb53d15a226356ec10aa5bf5e10c
ggujangi <asmwj3@naver.com>
2021-04-03 04:25:26 +0900

    [submit 2] #12925

--- src/solution/Ex_12925.java
+++ src/solution/Ex_12925.java
@@ -7,6 +7,18 @@ package solution;
 
 public class Ex_12925 {
     public int solution(String s) {
-        return Integer.parseInt(s);
+        boolean isMinus = false;
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '-') {
+                isMinus = true;
+            } else if (c != '+') {
+                result = result * 10 + (c - '0');
+            }
+        }
+
+        return (isMinus ? -1 : 1) * result;
     }
 }
