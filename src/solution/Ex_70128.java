6df2801c1d790456931050862ee5f192320ac414
ggujangi <asmwj3@naver.com>
2021-04-03 04:09:54 +0900

    [submit 1] #70128

---
+++ src/solution/Ex_70128.java
@@ -0,0 +1,16 @@
+package solution;
+
+/**
+ * #70128
+ * 내적
+ */
+
+public class Ex_70128 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
