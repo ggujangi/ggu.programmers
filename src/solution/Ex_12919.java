a7f8e279aa06bd335fc406ac53076a7b419cb188
ggujangi <asmwj3@naver.com>
2021-04-03 05:13:30 +0900

    [submit 2] #12919

--- src/solution/Ex_12919.java
+++ src/solution/Ex_12919.java
@@ -1,5 +1,7 @@
 package solution;
 
+import java.util.Arrays;
+
 /**
  * #12919
  * 서울에서 김서방 찾기
@@ -7,15 +9,7 @@ package solution;
 
 public class Ex_12919 {
     public String solution(String[] seoul) {
-        int result = 0;
-
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
-                result = i;
-                break;
-            }
-        }
-
+        int result = Arrays.asList(seoul).indexOf("Kim");
         return "김서방은 " + result + "에 있다";
     }
 }
