5ad7210d068b24775bf9188547172ae78b080b52
ggujangi <asmwj3@naver.com>
2021-04-04 04:19:56 +0900

    [submit 2] #12910

--- src/solution/Ex_12910.java
+++ src/solution/Ex_12910.java
@@ -1,8 +1,6 @@
 package solution;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * #12910
@@ -11,20 +9,8 @@ import java.util.List;
 
 public class Ex_12910 {
     public int[] solution(int[] arr, int divisor) {
-        List<Integer> list = new ArrayList<>();
-
-        for (int a : arr) {
-            if (a % divisor == 0) list.add(a);
-        }
-
-        if (list.size() == 0) return new int[]{-1};
-
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-        Arrays.sort(result);
-
+        int[] result = Arrays.stream(arr).sorted().filter(i -> i % divisor == 0).toArray();
+        if (result.length == 0) return new int[]{-1};
         return result;
     }
 }
