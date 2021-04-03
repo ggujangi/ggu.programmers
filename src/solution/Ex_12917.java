e9985d1757de9baec7b5e54ac840556f8a141068
ggujangi <asmwj3@naver.com>
2021-04-03 23:39:56 +0900

    [submit 1] #12917

---
+++ src/solution/Ex_12917.java
@@ -0,0 +1,17 @@
+package solution;
+
+import java.util.Arrays;
+
+/**
+ * #12917
+ * 문자열 내림차순으로 배치하기
+ */
+
+public class Ex_12917 {
+    public String solution(String s) {
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+
+        return new StringBuilder(new String(array)).reverse().toString();
+    }
+}
