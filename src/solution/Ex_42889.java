3542ec1abd56ce2156f6942d6c4234b7a13ffb0b
ggujangi <asmwj3@naver.com>
2021-04-13 02:48:59 +0900

    [submit 2] #42889

--- src/solution/Ex_42889.java
+++ src/solution/Ex_42889.java
@@ -1,5 +1,7 @@
 package solution;
 
+import java.util.Arrays;
+
 /**
  * #42889
  * 실패율
@@ -8,42 +10,43 @@ package solution;
 public class Ex_42889 {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        float[] f = new float[N];
         int count, total = stages.length;
 
-        for (int i = 1; i <= f.length; i++) {
+        Stage[] s = new Stage[N];
+
+        for (int i = 1; i <= answer.length; i++) {
             count = 0;
             for (int stage : stages) {
                 if (stage == i) {
                     count++;
                 }
             }
-            f[i - 1] = (float) count / (float) total;
-            answer[i - 1] = i;
+            s[i - 1] = new Stage(i, (float) count / (float) total);
             total = total - count;
         }
 
-        for (int i = 0; i < f.length; i++) {
-            float temp;
-            int tempIndex;
-            for (int j = 0; j < f.length; j++) {
-                if (f[i] > f[j]) {
-                    temp = f[j];
-                    f[j] = f[i];
-                    f[i] = temp;
-                    tempIndex = answer[j];
-                    answer[j] = answer[i];
-                    answer[i] = tempIndex;
-                } else if (f[i] == f[j]) {
-                    if (answer[i] < answer[j]) {
-                        tempIndex = answer[j];
-                        answer[j] = answer[i];
-                        answer[i] = tempIndex;
-                    }
-                }
+        Arrays.sort(s, (o1, o2) -> {
+            if (o1.result < o2.result) return 1;
+            else if (o1.result > o2.result) return -1;
+            else {
+                if (o1.id < o2.id) return -1;
             }
-        }
+            return 0;
+        });
 
+        for (int i = 0; i < s.length; i++) {
+            answer[i] = s[i].id;
+        }
         return answer;
+    }
+
+    class Stage {
+        int id;
+        float result;
+
+        public Stage(int id, float result) {
+            this.id = id;
+            this.result = result;
+        }
     }
 }
