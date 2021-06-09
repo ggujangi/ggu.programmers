f44f5eed45345cf63ceaf99e0afff5499072ef32
ggujangi <asmwj3@naver.com>
2021-06-09 14:02:36 +0900

    [submit 1] #12924

---
+++ src/solution/level_2/Ex_12924.java
@@ -0,0 +1,28 @@
+package solution.level_2;
+
+/**
+ * #12924
+ * url : https://programmers.co.kr/learn/courses/30/lessons/12924
+ *
+ * 숫자의 표현
+ * 효율성 테스트 : 불합격
+ */
+
+public class Ex_12924 {
+    public int solution(int n) {
+        int answer = 0;
+
+        int index = 1, sum = 0;
+
+        while (index <= n) {
+            for (int i = index; i <= n; i++) {
+                sum += i;
+                if (sum == n) answer++;
+            }
+            sum = 0;
+            index++;
+        }
+
+        return answer;
+    }
+}
